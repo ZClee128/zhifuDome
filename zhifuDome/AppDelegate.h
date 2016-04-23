f5e8dab6d73d5d1614c82f3e009b499f193018fb
mac <mac@mlt.local>
2016-04-23 09:12:20 +0800

    Initial Commit

---
+++ zhifuDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhifuDome
+//
+//  Created by mac on 16/4/23.
+//  Copyright © 2016年 lzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhifuDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhifuDome
+//
+//  Created by mac on 16/4/23.
+//  Copyright © 2016年 lzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
